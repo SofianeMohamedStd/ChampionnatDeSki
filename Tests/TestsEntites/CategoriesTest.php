bb62f1acd34b07789d4bf11d5e6c48f0bd0fbddf
SofianeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-10-21 14:09:35 +0200

    commit test

--- Tests/TestsEntites/CategoriesTest.php
+++ Tests/TestsEntites/CategoriesTest.php
@@ -42,7 +42,7 @@ it('test Set NomCategorie', function($name){
     })->with('Categorie');
         
 it("Test Exception Set NomCategorie",function($name) {
-    $this->categorie->setNomCategorie($name);
+    $this->categorie->etNomCategorie($name);
     })->with('CategorieEx')->throws(Exception::class);
 
 
