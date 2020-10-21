8ac7eac8569d5e75ac77c74f26f90e964bd6a71f
SofianeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-10-21 08:59:58 +0200

    repository, controller

--- src/Models/Participants.php
+++ src/Models/Participants.php
@@ -3,17 +3,22 @@ namespace App\Models;
 
 use App\Models\Model;
 use App\Entites\Participant;
+use App\Repository\ParticipantsRepository;
 
-class Participants extends Model
+
+class Participants implements ParticipantsRepository
 {
+    private object $db;
+
     public function __construct()
     {
-        $this->pdo = parent::getPdo();
+        $pdo = new Model();
+        $this->db = $pdo->getPDO();
     }
 
     public function AddParticipant(Participant $participant)
     {
-        $req = $this->pdo->prepare("INSERT INTO participants(categorie_id,profil_id,nom,prenom,date_de_naissance,photo,email) VALUE 
+        $req = $this->db->prepare("INSERT INTO participants(categorie_id,profil_id,nom,prenom,date_de_naissance,photo,email) VALUE 
         (?,?,?,?,?,?,?)");
         return $req->execute(array(
             $participant->getCategorieID(),
@@ -29,7 +34,7 @@ class Participants extends Model
 
     public function UpDateParticipant(Participant $participant)
     {
-        $req = $this->pdo->prepare("UPDATE participants SET (categorie_id = :categorie_id, profil_id = :profil_id, nom = :nom, prenom = :prenom,
+        $req = $this->db->prepare("UPDATE participants SET (categorie_id = :categorie_id, profil_id = :profil_id, nom = :nom, prenom = :prenom,
         date_de_naissance = :date_de_naissance, photo = :photo, email = :email WHERE id = :id");
         return $req->execute(array(
             'categorie_id' => $participant->getCategorieID(),
@@ -45,7 +50,7 @@ class Participants extends Model
 
     public function DeleteParticipant(Participant $participant)
     {
-        $req = $this->pdo->prepare('DELETE FROM participants where id = ?');
+        $req = $this->db->prepare('DELETE FROM participants where id = ?');
         return $req->execute(array(
             'id' => $participant->getId()
         ));
@@ -53,7 +58,7 @@ class Participants extends Model
    
     public function GetAllParticipants(): array
     {
-        $req = $this->pdo->prepare('SELECT * FROM participants');
+        $req = $this->db->prepare('SELECT * FROM participants');
         $req->execute();
 
         return $req->fetchAll();
@@ -61,7 +66,7 @@ class Participants extends Model
 
     public function GetOneParticipant(Participant $participant):array
     {
-        $req = $this->pdo->prepare('SELECT * From participants WHERE id = ?');
+        $req = $this->db->prepare('SELECT * From participants WHERE id = ?');
         $req->execute(array(
             'id' => $participant->getId()
         ));
--- src/Repository/ParticipantsRepository.php
+++ src/Repository/ParticipantsRepository.php
@@ -4,7 +4,13 @@
 namespace App\Repository;
 
 
+use App\Entites\Participant;
+
 interface ParticipantsRepository
 {
-
+    public function AddParticipant(Participant $participant);
+    public function UpDateParticipant(Participant $participant);
+    public function DeleteParticipant(Participant $participant);
+    public function GetAllParticipants(): array;
+    public function GetOneParticipant(Participant $participant):array;
 }
