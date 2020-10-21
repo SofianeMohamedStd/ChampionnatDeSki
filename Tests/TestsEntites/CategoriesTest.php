a80ca432258db2cd4152a4e58b1b5d5fa730cbb7
SofianeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-10-21 15:57:12 +0200

    commit phpUnit

--- Tests/TestsEntites/CategoriesTest.php
+++ Tests/TestsEntites/CategoriesTest.php
@@ -6,24 +6,25 @@ use Exception;
 use App\Entites\Categorie;
 use PHPUnit\Framework\TestCase;
 
-beforeEach(function () {
-    $this->categorie = new Categorie();
-});
+
 dataset('Categorie', ['senior', 'M','Mn']);
 dataset('CategorieEx', ['M1 n','M1_','M1/']);
 dataset('ListId',[1,2,3,4,5]);
 
 it('instance of',function() {
+    $this->categorie = new Categorie();
     $this->assertClassHasAttribute('id', Categorie::class);
     $this->assertClassHasAttribute('NomCategorie', Categorie::class);
 });
 
 it('has function Existe',function(){
+    $this->categorie = new Categorie();
     $this->expect(method_exists ($this->categorie,'setNomCategorie'))->toBeTrue();
     $this->expect(method_exists ($this->categorie,'getNomCategorie'))->toBeTrue();
     $this->expect(method_exists ($this->categorie,'getId'))->toBeTrue();
     });
 it('return', function($id){
+    $this->categorie = new Categorie();
     $stub = $this->createStub(Categorie::class);
     $stub->method('getId')
     ->willReturn($id);
@@ -31,17 +32,20 @@ it('return', function($id){
     })->with('ListId');
 
 it('should getNomString return String', function(){
+    $this->categorie = new Categorie();
     $this->categorie->setNomCategorie('Sofiane');
     $this->expect($this->categorie->getNomCategorie())->toBeString();
     });
 
 it('test Set NomCategorie', function($name){
+    $this->categorie = new Categorie();
     $res = $this->categorie->setNomCategorie($name);
     $this->expect($res->getNomCategorie())->toBeString();
     $this->assertMatchesRegularExpression(",^[a-zA-Z0-9[\].-]+$,",$name);
     })->with('Categorie');
         
 it("Test Exception Set NomCategorie",function($name) {
+    $this->categorie = new Categorie();
     $this->categorie->setNomCategorie($name);
     })->with('CategorieEx')->throws(Exception::class);
 
