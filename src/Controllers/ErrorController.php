73271e4ac8bd3664ce3f89005154ec5158bf7aee
SofianeMohamedStd <namoune.sofianemohamed@gmail.com>
2020-10-22 09:04:48 +0200

    changement du Model

---
+++ src/Controllers/ErrorController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controllers;
+
+use App\Controllers\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class ErrorController extends AbstractController
+{
+    
+    public function __invoke()
+    {
+        $this->twig = parent::getTwig();
+        header("HTTP/1.0 404 Not Found");
+        $pageTwig = 'errorView.html.twig';
+        $template = $this->twig->load($pageTwig);
+        return new Response($template->render());
+    }
+  
+   
+   
+}
